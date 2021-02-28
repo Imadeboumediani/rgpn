d8d29efe59ea6148924b67767ba855eac4898c8c
Imad Boumediani <boumediani.pro@gmail.com>
2021-02-28 23:57:50 +0100

    deconnexion

---
+++ src/composants/navigation/Navigation.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navigation = () => {
+    return(
+        <nav style={{display: 'flex' , justifyContent : 'flex-end'}}>
+          <p className='f3 link dim grey underline pa3 pointer'>Deconnexion</p>  
+        </nav>
+    )
+}
+
+export default Navigation ;
