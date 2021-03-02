288e9abc268ea4d44c4b35dae03146fc0d2a315a
Imad Boumediani <boumediani.pro@gmail.com>
2021-03-02 21:15:42 +0100

    test numero 1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
 import Navigation from './composants/navigation/Navigation' ;
+import Logo from './composants/Logo/Logo' ;
+import Connexion from './composants/Connexion/Connexion' ;
+
+
 
 function App() {
   return (
     <div className="App">
      <Navigation />
+     <Logo />
+     <Connexion/>
      
     </div>
   );
