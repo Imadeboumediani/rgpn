3249631ad1881d4cc90a7b9f94c8947b969c9f0b
MehdiCHERRADI <mehdicherradi1994@gmail.com>
2021-03-03 04:35:02 +0100

    affichage d'une up dans une nouvelle page

--- src/App.js
+++ src/App.js
@@ -1,17 +1,36 @@
 import './App.css';
+import { 
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom"
 import Navigation from './composants/navigation/Navigation' ;
 import Logo from './composants/Logo/Logo' ;
 import Connexion from './composants/Connexion/Connexion' ;
+import Unitep from './composants/Unitep/Unitep' ;
+
 
 
 
 function App() {
   return (
     <div className="App">
-     <Navigation />
-     <Logo />
-     <Connexion/>
-     
+    <Router>
+      <Switch>
+        <Route exact path ="/rgpn">
+          <Navigation />
+          <Logo />
+          <Connexion/>
+        </Route>
+    
+      
+        <Route path ="/Unitep">
+          <Unitep/> 
+        </Route>
+      </Switch>
+    </Router>
+        
+      
     </div>
   );
 }
